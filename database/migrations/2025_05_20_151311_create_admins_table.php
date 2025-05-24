168a84d0a0f6ae2894ea61fee9955ec5c3e350c3
aseelbuker <aseel_3679@limu.edu.ly>
2025-05-24 17:56:07 +0200

    message

---
+++ database/migrations/2025_05_20_151311_create_admins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table-> A_Id();
+            $table-> A_name();
+            $table-> A_email();
+            $table-> A_password();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
