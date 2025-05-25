a4ed00016a2b0d66b85b8f843912f3c59b41a197
RADWAN <radwantareq66@gmail.com>
2025-05-25 15:34:46 +0200

    admin init

--- database/migrations/2025_05_25_133232_create_admins_table.php
+++ database/migrations/2025_05_25_133232_create_admins_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("email")->unique();
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('admins');
     }
 };
